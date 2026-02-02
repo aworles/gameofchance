c96b42af4f7dab50cdbdd7ae513c622c287054b6
aworles <khanhanna.vu2020@gmail.com>
2026-02-02 09:34:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,12 @@
 // ------------------------------
 
 let balance = 100;
-const goal = 500; // ← CHANGE YOUR GOAL HERE
+const goal = 402; // chip goal
 let goalCode = "---";
+
+// streak goals
+let numberStreak = 0;
+let colorStreak = 0;
 
 const colors = ["red", "white", "black"];
 
@@ -126,33 +130,53 @@ playBtn.addEventListener("click", () => {
   const correctColor = randomColor();
   const correctNumber = randomNumber();
 
+  let wonNumber = false;
+  let wonColor = false;
+
   // WINNING LOGIC
   if (userColor === correctColor && userNumber === correctNumber) {
     balance += bet * 3;
     log(`JACKPOT! You hit BOTH! +${bet * 3} tabs`);
+    wonNumber = true;
+    wonColor = true;
   } else if (userColor === correctColor) {
     const win = Math.floor(bet * 1.5);
     balance += win;
     log(`Correct color! +${win} tabs`);
+    wonColor = true;
   } else if (userNumber === correctNumber) {
     balance += bet * 2;
     log(`Correct number! +${bet * 2} tabs`);
+    wonNumber = true;
   } else {
     log(`No win this round.`);
   }
+
+  // UPDATE STREAKS
+  numberStreak = wonNumber ? numberStreak + 1 : 0;
+  colorStreak = wonColor ? colorStreak + 1 : 0;
 
   updateBalance();
   updateGoalBar();
 
   // SHOW CORRECT ANSWERS
   message.innerHTML += `<br><span style="opacity:0.7;">Correct: ${correctColor}, ${correctNumber}</span>`;
+  message.innerHTML += `<br><span style="opacity:0.7;">Number streak: ${numberStreak} | Color streak: ${colorStreak}</span>`;
 
-  // GOAL REACHED
-  if (balance >= goal && goalCode === "---") {
+  // ------------------------------
+  // GOAL CHECK (ANY OF THE THREE)
+  // ------------------------------
+
+  const goalReached =
+    balance >= 402 ||
+    numberStreak >= 3 ||
+    colorStreak >= 6;
+
+  if (goalReached && goalCode === "---") {
     baguetteConfetti();
     goalCode = "49201"; // your code
     updateGoalCode();
-    log("🎉 You've reached your goal! Code unlocked!");
+    log("🎉 You've completed a goal! Code unlocked!");
   }
 
   // GAME OVER
@@ -168,6 +192,8 @@ playBtn.addEventListener("click", () => {
 restartBtn.addEventListener("click", () => {
   balance = 100;
   goalCode = "---";
+  numberStreak = 0;
+  colorStreak = 0;
   updateBalance();
   updateGoalBar();
   updateGoalCode();
